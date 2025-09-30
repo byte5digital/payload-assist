bf68e9c0f081a60704ad74cfda038c72b9a630eb
Moses <50459836+justanothermoses@users.noreply.github.com>
2025-09-30 10:48:56 +0200

    chore: add PayloadAssistError (#31)
    Co-authored-by: Moses <motten@byte5.de>

--- src/utils/transform-and-validate.ts
+++ src/utils/transform-and-validate.ts
@@ -1,8 +1,12 @@
 import { Dto } from "../types/dto";
 import { payloadAssistConfig as payloadAssistConfig } from "./payload-assist";
+import PayloadAssistError from "../types/PayloadAssistError";
 
 export const transformAndValidate = (dto: new () => Dto, data: unknown) => {
-  if (!payloadAssistConfig) throw `PayloadAssist is not initialized. Use payloadAssist() to initialize it.`;
+  if (!payloadAssistConfig)
+    throw new PayloadAssistError(
+      `PayloadAssist is not initialized. Use payloadAssist() to initialize it.`
+    );
 
   return payloadAssistConfig.transformAndValidate(dto, data);
 };
